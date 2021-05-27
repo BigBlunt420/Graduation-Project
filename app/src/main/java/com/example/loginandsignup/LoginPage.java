4d57db68baef3759dad89149fc3b8c76651fc31c
chiaying-lin <linjoy1999@gmail.com>
2021-05-27 17:38:17 +0800

    模板完成

--- app/src/main/java/com/example/loginandsignup/LoginPage.java
+++ app/src/main/java/com/example/loginandsignup/LoginPage.java
@@ -2,7 +2,10 @@ package com.example.loginandsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page2);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
