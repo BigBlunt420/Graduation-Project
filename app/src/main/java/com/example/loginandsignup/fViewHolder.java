521d5d8eec83f5e28480ea483981782fef8bcb36
good2beanerrrd <sleepwithonenostrilopen@gmail.com>
2021-11-22 11:32:48 +0800

    friend

---
+++ app/src/main/java/com/example/loginandsignup/fViewHolder.java
@@ -0,0 +1,50 @@
+package com.example.loginandsignup;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class fViewHolder extends RecyclerView.ViewHolder {
+
+    TextView Name, Phone;
+    View view;
+
+    public fViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+
+        view = itemView;
+
+        //item click
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClickListener.onItemClick(v, getAdapterPosition());
+            }
+        });
+
+        //item long click
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClickListener.onItemLongClick(v, getAdapterPosition());
+                return false;
+            }
+        });
+
+        Name = itemView.findViewById(R.id.all_users_profile_full_name);
+        Phone = itemView.findViewById(R.id.all_users_profile_phone_number);
+
+    }
+    private fViewHolder.ClickListener ClickListener;
+    public interface ClickListener{
+        void onItemClick(View view,int position);
+        void onItemLongClick(View view,int position);
+    }
+    public void setOnClickListener(fViewHolder.ClickListener clickListener){
+        ClickListener = clickListener;
+    }
+}
